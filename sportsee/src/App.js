58d208b2be4e179cc3b78e5a13b1c9045ae8e5e7
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2024-04-15 17:44:02 +0200

    creation bar chart

--- sportsee/src/App.js
+++ sportsee/src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
-function App() {
+const USER_ACTIVITY = [
+  {
+    sessions: [
+      {
+        day: "2020-07-01",
+        kilogram: 80,
+        calories: 240,
+      },
+      {
+        day: "2020-07-02",
+        kilogram: 80,
+        calories: 220,
+      },
+      {
+        day: "2020-07-03",
+        kilogram: 81,
+        calories: 280,
+      },
+      {
+        day: "2020-07-04",
+        kilogram: 81,
+        calories: 290,
+      },
+      {
+        day: "2020-07-05",
+        kilogram: 80,
+        calories: 160,
+      },
+      {
+        day: "2020-07-06",
+        kilogram: 78,
+        calories: 162,
+      },
+      {
+        day: "2020-07-07",
+        kilogram: 76,
+        calories: 390,
+      },
+    ],
+  },
+];
+
+
+export default function App() {
+  // Créer un tableau de données avec des numéros séquentiels pour l'axe x
+  const data = USER_ACTIVITY[0].sessions.map((session, index) => ({
+    number: index + 1,
+    kilogram: session.kilogram,
+    calories: session.calories,
+  }));
+  // Trouver la valeur maximale des calories pour ajuster l'échelle de l'axe y
+  const maxCalories = Math.max(...data.map((item) => item.calories));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BarChart
+      width={800}
+      height={500}
+      data={data}
+      margin={{ top: 80, right: 48, bottom: 32, left: 48 }}
+      barGap={8}
+      barCategoryGap="20%"
+    >
+      <CartesianGrid vertical={false} strokeDasharray="2" />
+      <XAxis
+        dataKey="number" // Utiliser le numéro séquentiel comme valeur pour l'axe x
+        dy={16}
+        tickLine={false}
+        tick={{ fontSize: 14, fontWeight: 500 }}
+      />
+      <YAxis
+        dataKey="kilogram"
+        orientation="right"
+        axisLine={false}
+        tickLine={false}
+        allowDecimals={false}
+      />
+      <YAxis
+        yAxisId="right"
+        orientation="left"
+        axisLine={false}
+        tickLine={false}
+        allowDecimals={false}
+        domain={[0, maxCalories]} // Définir le domaine de l'axe y pour les calories
+        hide={true}
+      />
+      <Tooltip />
+      <Legend />
+      <Bar radius={[20, 20, 0, 0]} maxBarSize={8} dataKey="kilogram" />
+      <Bar
+        radius={[20, 20, 0, 0]}
+        maxBarSize={8}
+        dataKey="calories"
+        fill="red"
+        yAxisId="right" // Utiliser l'axe y de droite pour les calories
+      />
+    </BarChart>
   );
-}
-
-export default App;
+};
